9d5ffad7297de0ad073a7eb6ca0b6ed47de88116
Shruti Sood <sood0025@algonquinlive.com>
2024-07-16 14:31:05 -0400

    changes

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import Footer from '../components/footer';
-import TitleBar from '../components/TitleBar.js';
+import TitleBar from '../components/TitleBar';
+import Welcome from '../components/AboutUs/Welcome';
+import OurStory from '../components/AboutUs/OurStory';
+import OurServices from '../components/AboutUs/OurServices';
+import OurApproach from '../components/AboutUs/OurApproach';
+import OurCommitment from '../components/AboutUs/OurCommitment';
+
+
+
 
 const AboutUs = () => {
   return (
     <>
-
       <div>
+        <TitleBar colorCode="#F7962D" titleText="About Us" />
         <h1 class='titleBar'>About Us</h1>
 
 
-        <p>Please add your page stuff here</p>
-
-        <TitleBar colorCode="#14ACA0" titleText="About Us"></TitleBar>
         
       </div>
-      
-      
-
+      <Welcome />
+      <OurStory />
+      <OurServices />
+      <OurApproach />
+      <OurCommitment />
       <Footer />
-
+      
+    
     </>
   );
 };

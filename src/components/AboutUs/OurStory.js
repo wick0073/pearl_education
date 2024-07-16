0a6fa444a9ecc79f81dc0de4239f66152bacf4e0
Shruti Sood <sood0025@algonquinlive.com>
2024-07-16 14:22:29 -0400

    about us components

---
+++ src/components/AboutUs/OurStory.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../css/AboutUs/OurStory.css';
+
+const OurStory = () => {
+  return (
+    <div className="our-story-section">
+      <h1>Our Story</h1>
+      <div className="our-story-container">
+        <div className="our-story-content">
+          <p>
+            Founded with a passion for education and a commitment to excellence, Pearl Education has grown into a trusted name in the tutoring industry. Our team of experienced and dedicated educators is driven by the desire to foster a love for learning and to help students achieve their academic aspirations. Over the years, we have earned a reputation for providing personalized and effective tutoring that makes a real difference in students' lives.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OurStory;
